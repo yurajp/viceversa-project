d5233c8517bc4adab96f6effee7bb3a2f5de0d18
Yuriy <81621025+yurajp@users.noreply.github.com>
2022-01-24 02:18:46 +0300

    Final for this Lesson

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name="home"),
+    path('reverse/', views.reverse, name='reverse')
 ]
--- viceversa/views.py
+++ viceversa/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def reverse(request):
+    reverse_text = request.GET['usertext'][::-1]
+    return render(request, 'reverse.html', {'reverse_text': reverse_text})
